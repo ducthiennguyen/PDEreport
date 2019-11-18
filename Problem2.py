ab6df93d243406e99b1bbefc0ee05dd1b1055e92
ducthiennguyen <nguyenducthien23071999@gmail.com>
2019-11-18 23:03:06 +0900

    Problem2update

--- Problem2.py
+++ Problem2.py
@@ -5,9 +5,9 @@ import matplotlib.pyplot as plt
 #u[i][j] += (v / (dx**2.) * (u[i+1][j] + u[i-1][j] + u[i][j+1] + u[i][j-1]) - a / dx * (2*u[i][j] -u[i-1][j] - u[i][j-1])) * dt
 
 a = 1
-v = 0.1
+v = 0.02
 L = 10
-dx = 0.8
+dx = 0.2
 dy = dx
 n = int(L/dx)
 dt = 0.1
@@ -17,15 +17,12 @@ x = np.linspace(0, L, n)
 y = np.linspace(0, L, n)
 
 u = np.zeros((n, n))
-u[:, 0] = 0.75
-u[:, n-1] = 0.30
-u[0, :] = 2.37
-u[20:30, 20:30] = 10.0
+u[:, 0] = 0.1
+u[:, n-1] = 0.2
+u[0, :] = 0.3
+u[7:9, 7:9] = 2.37
 
 for k in range(nt):
-    for i in range(1, n-1):
-        for j in range(1, n-1):
-            u[i][j] += (v / (dx**2.) * (u[i+1][j] + u[i-1][j] + u[i][j+1] + u[i][j-1] - 4*u[i][j]) - (u[i][j] / dx) * (2*u[i][j] - u[i-1][j] - u[i][j-1])) * dt
 
     plt.contourf(x, y, u, cmap = 'rainbow')
     plt.colorbar()
@@ -34,3 +31,9 @@ for k in range(nt):
     plt.clf()
     plt.cla()
 
+    # for i in range(1, n-1):
+    #     for j in range(1, n-1):
+    #         u[i][j] += (v / (dx**2.) * (u[i+1][j] + u[i-1][j] + u[i][j+1] + u[i][j-1] - 4*u[i][j]) - (u[i][j] / dx) * (2*u[i][j] - u[i-1][j] - u[i][j-1])) * dt
+
+    u = u + (v / (dx**2.) * (np.roll(u, -1, axis = 0) + np.roll(u, 1, axis = 0) + np.roll(u, -1, axis = 1) + np.roll(u, 1, axis = 1) - 4*u) - \
+    (a / dx) * (2*u - np.roll(u, 1, axis = 0) - np.roll(u, 1, axis = 1))) * dt
