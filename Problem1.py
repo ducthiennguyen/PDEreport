9ce1da45b60d93a0c74dcebfd4b1a47f48d82d22
ducthiennguyen <nguyenducthien23071999@gmail.com>
2019-11-19 10:21:00 +0900

    Problem1update

--- Problem1.py
+++ Problem1.py
@@ -19,16 +19,16 @@ T[:, n-1] = 30
 T[0, :] = 237
 T[20:30, 20:30] = 400
 
-for k in range(15001):
-    r = 1/4 * (T[2:n, 1:n-1] + T[0:n-2, 1:n-1] + T[1:n-1, 2:n] + T[1:n-1, 0:n-2] - (dx**2.)*g/alpha) - T[1:n-1, 1:n-1]
-    T[1:n-1, 1:n-1] += (1-w) * T[1:n-1, 1:n-1] + w*r
-    if k % 500 != 0: continue
-    plt.contourf(x, y, T, cmap = 'rainbow', vmin=-1400, vmax=400)
-    plt.colorbar()
-    plt.title('Iteration%d'%(k))
-    plt.savefig('GaussSeidel%d.png'%(k))
-    plt.clf()
-    plt.cla()
+# for k in range(15001):
+#     r = 1/4 * (T[2:n, 1:n-1] + T[0:n-2, 1:n-1] + T[1:n-1, 2:n] + T[1:n-1, 0:n-2] - (dx**2.)*g/alpha) - T[1:n-1, 1:n-1]
+#     T[1:n-1, 1:n-1] += (1-w) * T[1:n-1, 1:n-1] + w*r
+#     if k % 500 != 0: continue
+#     plt.contourf(x, y, T, cmap = 'rainbow', vmin=-1400, vmax=400)
+#     plt.colorbar()
+#     plt.title('Iteration%d'%(k))
+#     plt.savefig('GaussSeidel%d.png'%(k))
+#     plt.clf()
+#     plt.cla()
 
 #Investigate various boundary conditions (non-steady state) by discussing its effects using animation
 
@@ -54,15 +54,37 @@ T[10:20, 20:30] = 237
 
 def fdm(T):
 #    Top row
-    T[grid_x-1, 1:grid_y-1] += d * (T[grid_x-2, 1:grid_y-1] + T[grid_x-2, 1:grid_y-1] + T[grid_x-1, 2:grid_y] + T[grid_x-1, 0:grid_y-2] - 4 * T[grid_x-1, 1:grid_y-1])
+    # T[grid_x-1, 1:grid_y-1] += d * (T[grid_x-2, 1:grid_y-1] + T[grid_x-2, 1:grid_y-1] + T[grid_x-1, 2:grid_y] + T[grid_x-1, 0:grid_y-2] - 4 * T[grid_x-1, 1:grid_y-1])
 #    Right column
-    T[1:grid_x-1, grid_y-1] += d * (T[1:grid_x-1, grid_y-2] + T[1:grid_x-1, grid_y-2] + T[2:grid_x, grid_y-1] + T[0:grid_x-2, grid_y-1] - 4 * T[1:grid_x-1, grid_y-1])
+    # T[1:grid_x-1, grid_y-1] += d * (T[1:grid_x-1, grid_y-2] + T[1:grid_x-1, grid_y-2] + T[2:grid_x, grid_y-1] + T[0:grid_x-2, grid_y-1] - 4 * T[1:grid_x-1, grid_y-1])
 #    Bottom row
-    T[0, 1:grid_y-1] += d * (T[1, 1:grid_y-1] + T[1, 1:grid_y-1] + T[0, 2:grid_y] + T[0, 0:grid_y-2] - 4 * T[0, 1:grid_y-1])
-#    Left column
-    T[1:grid_x-1, 0] += d * (T[1:grid_x-1, 1] + T[1:grid_x-1, 1] + T[2:grid_x, 0] + T[0:grid_x-2, 0] - 4 * T[1:grid_x-1, 0])
-    
+    # T[0, 1:grid_y-1] += d * (T[1, 1:grid_y-1] + T[1, 1:grid_y-1] + T[0, 2:grid_y] + T[0, 0:grid_y-2] - 4 * T[0, 1:grid_y-1])
+# #    Left column
+    # T[1:grid_x-1, 0] += d * (T[1:grid_x-1, 1] + T[1:grid_x-1, 1] + T[2:grid_x, 0] + T[0:grid_x-2, 0] - 4 * T[1:grid_x-1, 0])
+
     T[1:grid_x-1, 1:grid_y-1] += d * (T[2:grid_x, 1:grid_y-1] + T[0:grid_x-2, 1:grid_y-1] + T[1:grid_x-1, 2:grid_y] + T[1:grid_x-1, 0:grid_y-2] - 4 * T[1:grid_x-1, 1:grid_y-1])
-    
+
     return T
 
+plt.contourf(x, y, T, cmap = 'rainbow')
+plt.colorbar()
+plt.title('Initial distribution')
+plt.savefig('Initial.png')
+plt.clf()
+plt.cla()
+
+for t in range(0, nt):
+    T = fdm(T)
+    # plt.contourf(x, y, T, cmap = 'rainbow')
+    # plt.colorbar()
+    # plt.title('Time %.5f'%(t*dt))
+    # plt.savefig('GaussSeidel%d.png'%(t))
+    # plt.clf()
+    # plt.cla()
+
+plt.contourf(x, y, T, cmap = 'rainbow')
+plt.colorbar()
+plt.title('Time %.3f distribution'%((nt-1)*dt))
+plt.savefig('Dirichlet%d'%(nt-1))
+plt.clf()
+plt.cla()
